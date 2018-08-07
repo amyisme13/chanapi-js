09928577a0733c9c5ab468cd00c49df5a596cd23
Azmi Makarima <amy.azmim@gmail.com>
2018-08-08 02:02:53 +0800

    add utils

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,16 @@ const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
 const chanApiBaseUrl = 'https://a.4cdn.org/';
 const chanBaseUrl = 'https://boards.4chan.org';
 
-export default axios.create({
+export const chanAxios = axios.create({
   baseURL: `${corsAnywhere}${chanApiBaseUrl}`,
   headers: { 'X-Requested-With': chanBaseUrl }
 });
+
+export const reverseHtmlSpecialChars = (str: string): string => {
+  return str
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'");
+};
