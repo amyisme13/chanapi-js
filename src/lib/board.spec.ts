badf91b2ce7e544767aa746a0a8e350c70aba6c0
Azmi Makarima <amy.azmim@gmail.com>
2018-08-10 19:42:54 +0800

    add throw tests for Board.getThreads

--- src/lib/board.spec.ts
+++ src/lib/board.spec.ts
@@ -4,13 +4,16 @@ import { Board } from './board';
 const boardCode = 'g';
 const board = new Board(boardCode);
 
+const invalidBoardCode = 'asdasd';
+const invalidBoard = new Board(invalidBoardCode);
+
 test('method getThreads without page should not throws', async t => {
   await t.notThrows(board.getThreads());
 });
 
 test('method getThreads without page should not be empty', async t => {
-  const boards = await board.getThreads();
-  t.not(boards.size, 0);
+  const threads = await board.getThreads();
+  t.not(threads.size, 0);
 });
 
 test('method getThreads with page should not throws', async t => {
@@ -18,6 +21,14 @@ test('method getThreads with page should not throws', async t => {
 });
 
 test('method getThreads with page should not be empty', async t => {
-  const boards = await board.getThreads(1);
-  t.not(boards.size, 0);
+  const threads = await board.getThreads(1);
+  t.not(threads.size, 0);
+});
+
+test('method getThreads of invalid board without page should throw', async t => {
+  await t.throws(invalidBoard.getThreads());
+});
+
+test('method getThreads of invalid board with page should throw', async t => {
+  await t.throws(invalidBoard.getThreads(1));
 });
