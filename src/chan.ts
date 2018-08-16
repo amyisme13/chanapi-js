71ea72c0ac13961b76c968a3f3909e9487cf840b
Azmi Makarima <amy.azmim@gmail.com>
2018-08-17 00:42:36 +0800

    better Chan

---
+++ src/chan.ts
@@ -0,0 +1,38 @@
+import { AxiosInstance } from 'axios';
+import { chanAxios } from './utils';
+
+import { getBoards } from './lib/boards';
+import { getThread } from './lib/thread';
+import { getCatalog, getThreads } from './lib/threads';
+
+export class Chan {
+  private axios: AxiosInstance;
+
+  constructor(options: Option = {}) {
+    this.axios = options.axios || chanAxios;
+  }
+
+  public get({ board, page, thread }: Parameters = {}) {
+    if (board && thread) {
+      return getThread(this.axios, board, thread);
+    } else if (board && page) {
+      return getThreads(this.axios, board, page);
+    } else if (board) {
+      return getCatalog(this.axios, board);
+    } else {
+      return getBoards(this.axios);
+    }
+  }
+}
+
+export const chan = new Chan();
+
+interface Option {
+  axios?: AxiosInstance;
+}
+
+interface Parameters {
+  board?: string;
+  page?: number;
+  thread?: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export * from './lib/boards';
-export * from './lib/board';
-export * from './lib/threads';
-export * from './lib/thread';
-export * from './lib/post';
+export * from './chan';
